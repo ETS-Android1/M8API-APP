65f821b8fa001285f5663e81aca177a468902c39
Adam <cf19adan.montesinos@iesjoandaustria.org>
2021-11-26 17:51:23 +0100

    adam

--- app/src/main/java/com/example/m8api_app/FragmentHome.java
+++ app/src/main/java/com/example/m8api_app/FragmentHome.java
@@ -3,6 +3,7 @@ package com.example.m8api_app;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -31,28 +32,28 @@ public class FragmentHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View viewForm = inflater.inflate(R.layout.fragment_home, container, false);
+        View viewHome = inflater.inflate(R.layout.fragment_home, container, false);
 
-        Button BEspaña = viewForm.findViewById(R.id.idiom1);
-        Button BCatala = viewForm.findViewById(R.id.idiom2);
-        Button BAngles = viewForm.findViewById(R.id.idiom3);
-        Button BNoLogin = viewForm.findViewById(R.id.idiom4);
+        Button BEspaña = viewHome.findViewById(R.id.idiom1);
+        Button BCatala = viewHome.findViewById(R.id.idiom2);
+        Button BAngles = viewHome.findViewById(R.id.idiom3);
+        Button BNoLogin = viewHome.findViewById(R.id.idiom4);
 
         BEspaña.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-               // prefs.edit().putString("language", "en").commit();
+                Save("Es");
             }
         });
 
         BCatala.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-             //Hay  que poner algo que active
+                Save("Ca");
             }
         });
 
         BAngles.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                //Hay  que poner algo que active
+                Save("En");
             }
         });
 
@@ -67,7 +68,18 @@ public class FragmentHome extends Fragment {
             }
         });
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return viewHome;
+    }
+
+    public void Save(String locale){
+        SharedPreferences preferences = getActivity().getSharedPreferences("MisPreferencias", Context.MODE_PRIVATE);
+        final Configuration config = new Configuration(getResources().getConfiguration());
+        config.locale = new Locale(locale);
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("Idioma",locale );
+        editor.commit();
+        refresh();
     }
 
     public void refresh(){
